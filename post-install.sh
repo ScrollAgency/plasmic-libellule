5c9ab77f67046d32f805c71c919d55b337d454dc
Gael <prigentgael@gmail.com>
2025-05-19 15:21:34 +0200

    ✨ Simplify installation

--- post-install.sh
+++ post-install.sh
@@ -9,6 +9,8 @@ LIBRARY_DIR="."
 if [ ! -d "$PROJECT_DIR2/libellule/" ]; then
   mkdir -p "$PROJECT_DIR2/libellule/"
   cp -r "$LIBRARY_DIR/libellule/." "$PROJECT_DIR2/libellule/"
+  cp -r "$LIBRARY_DIR/libellule/.env.install.model" "$PROJECT_DIR2/.env.install"
+  cp -r "$LIBRARY_DIR/libellule/package.json.model" "$PROJECT_DIR2/package.json"
 fi
 
 echo "✅  Installation réussie !"
